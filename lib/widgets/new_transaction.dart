fa6f9f4e54bad0dfd774199005865bf76c0becc8
Dipesh Goswami <dipeshpuri9@gmail.com>
2021-03-22 12:02:04 -0400

    Add new Transction

---
+++ lib/widgets/new_transaction.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import '../widgets/new_transaction.dart';
+
+class NewTransaction extends StatelessWidget {
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+  final Function addTx;
+
+  NewTransaction(this.addTx);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            TextField(
+              decoration: InputDecoration(labelText: 'Title'),
+              controller: titleController,
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Amount'),
+              controller: amountController,
+            ),
+            FlatButton(
+              child: Text('Add Transaction'),
+              textColor: Colors.green,
+              onPressed: () {
+                addTx(
+                    titleController.text, double.parse(amountController.text));
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
